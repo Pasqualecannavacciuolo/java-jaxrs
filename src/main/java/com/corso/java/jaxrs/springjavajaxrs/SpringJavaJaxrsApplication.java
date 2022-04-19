0f04cb1a3555ee5bebc62a7e0e87451a1d69da42
Pasquale <p.cannavacciuolo@icloud.com>
2022-04-19 10:00:21 +0200

    Initial commit

---
+++ src/main/java/com/corso/java/jaxrs/springjavajaxrs/SpringJavaJaxrsApplication.java
@@ -0,0 +1,13 @@
+package com.corso.java.jaxrs.springjavajaxrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJavaJaxrsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJavaJaxrsApplication.class, args);
+    }
+
+}
